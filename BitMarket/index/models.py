c7e2e9ca89fef412dabe31e5c2d9e4ce208ff4b7
Chrystian Kisło <chrystian.kislo@gmail.com>
2014-04-19 11:50:23 +0200

    TEST Z APTANY

--- BitMarket/index/models.py
+++ BitMarket/index/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+# Create your models here. TEST
 class UserProfile(models.Model):
     user = models.ForeignKey(User, unique=True)
     url = models.URLField("Website", blank=True)
