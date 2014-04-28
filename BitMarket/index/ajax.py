5e89589f9ee57d1ad83c5553e130ccef1b6e07c6
Chrystian Kisło <chrystian.kislo@gmail.com>
2014-04-28 22:30:38 +0200

    dodanie aplikacji wallet.

--- BitMarket/index/ajax.py
+++ BitMarket/index/ajax.py
@@ -1,6 +1,7 @@
 from django.utils import simplejson
 from dajaxice.decorators import dajaxice_register
 
+
 @dajaxice_register
 def sayhello(request):
     return simplejson.dumps({'message':'Hello World'})
