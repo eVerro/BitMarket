f41c49c45d467493bae63453089a1d26fa8a4dc3
unknown <chyba ja@.(none)>
2014-06-07 23:39:20 +0200

    poprawka widoku user

--- BitMarket/index/views.py
+++ BitMarket/index/views.py
@@ -50,7 +50,8 @@ def ltc_view(request):
 
 def user(request):
     wallets = UserWallet.objects.filter(user=request.user)
-    userhistories = UserProxy.getCommissionHistory(self, sort=None)
+    userproxy=UserProxy.objects.get(id=request.user.id)
+    userhistories = UserProxy.getCommissionHistory( userproxy, sort=None)
     local = locals()
     return render_to_response('user/user.html', {'local': local})
 
