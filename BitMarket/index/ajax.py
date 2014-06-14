3caeea0c91afd38f5404b7ef976e742afa0509cd
WPiotr <p.wyszkowski92@gmail.com>
2014-06-14 23:54:33 +0200

    a

--- BitMarket/index/ajax.py
+++ BitMarket/index/ajax.py
@@ -77,7 +77,7 @@ def createTable(request, left_currency, right_currency):
     for comm in Commission.getCommissions(cryptocurrency_sold=right_currency,cryptocurrency_bought=left_currency, sort='destination_price'):
         left_table+='<tr>'
         left_table+='<td>'
-        left_table+=str(format(Decimal(comm.destination_amount),'.10f'))
+        left_table+=str(format(Decimal(comm.destination_price),'.10f'))
         left_table+='</td>'
         left_table+='<td>' 
         left_table+=str(format(Decimal(comm.source_amount),'.10f'))
@@ -97,10 +97,10 @@ def createTable(request, left_currency, right_currency):
     right_table+='<thead><tr><th>Cena('+str(left_currency)+')</th><th>'+str(right_currency)+'</th><th>Razem('+str(left_currency)+')</th>'
     right_table+='<th></th>'
     right_table+='</tr></thead>'
-    for comm in Commission.getCommissions(cryptocurrency_sold=left_currency,cryptocurrency_bought=right_currency, sort='destination_price'):
+    for comm in Commission.getCommissions(cryptocurrency_sold=left_currency,cryptocurrency_bought=right_currency, sort='source_price'):
         right_table+='<tr>'
         right_table+='<td>'
-        right_table+=str(format(Decimal(comm.destination_price),'.10f'))
+        right_table+=str(format(Decimal(comm.source_price),'.10f'))
         right_table+='</td>'
         right_table+='<td>'
         right_table+=str(format(Decimal(comm.destination_amount),'.10f'))
